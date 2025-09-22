e07ef9c0c69b4251a26da8d8423580bc89d88170
Jhonatan <jhonatan.moraes957@gmail.com>
2025-09-22 13:15:12 -0300

    aulas revisao

---
+++ Aula016.js
@@ -0,0 +1,11 @@
+let variavelGlobal = 'Sou Global'
+
+    function minhaFuncao(){
+        let variavelLocal = 'Sou local'
+        console.log(variavelGlobal);
+        console.log(variavelLocal);
+    }
+    minhaFuncao();
+    console.log(variavelGlobal);
+    console.log(variavelLocal);
+    
---
+++ aula015.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+function BemVindo(){
+    
+    console.log('----------------------');
+    console.log('bem vindo a UC Backend');
+    console.log('----------------------');
+    
+}
+BemVindo();
+
+for (let i = 0; i <= 1000000000000000000000; i++) {
+    BemVindo();
+}
